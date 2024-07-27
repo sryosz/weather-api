7a6259eb138e028d6071bf8c3c5c33f62bcd40bc
sryosz <workbin.osz@gmail.com>
2024-07-28 00:37:21 +0300

    Add formatting & Delete ToString Add function for formatting decoded json data

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,12 +1,6 @@
 package models
 
-import "fmt"
-
 type WeatherData struct {
 	Elevation float64        `json:"elevation"`
 	Hourly    map[string]any `json:"hourly"`
-}
-
-func (d *WeatherData) ToString() string{
-	return fmt.Sprintf("%v%v", d.Elevation, d.Hourly)
 }
--- internal/sender/emailsender.go
+++ internal/sender/emailsender.go
@@ -36,9 +36,15 @@ func (s *EmailSender) Send(data *models.WeatherData) error {
 	auth := smtp.PlainAuth("", s.emailFrom, s.password, s.host)
 
 	to := []string{s.emailTo}
-	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", to[0], s.subject, data.ToString()))
 
-	err := smtp.SendMail(fmt.Sprintf("%s:%s", s.host, s.port), auth, s.emailFrom, to, msg)
+	parsedData, err := formatMsg(data)
+	if err != nil {
+		return err
+	}
+
+	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", to[0], s.subject, parsedData))
+
+	err = smtp.SendMail(fmt.Sprintf("%s:%s", s.host, s.port), auth, s.emailFrom, to, msg)
 	if err != nil {
 		return err
 	}
@@ -46,4 +52,32 @@ func (s *EmailSender) Send(data *models.WeatherData) error {
 	log.Info(fmt.Sprintf("sent msg from %s to %s", s.emailFrom, s.emailTo))
 
 	return nil
+}
+
+func formatMsg(data *models.WeatherData) (string, error) {
+
+	msg := fmt.Sprintf("Weather at %.2fm\n", data.Elevation)
+
+	timeSlice, ok := data.Hourly["time"].([]any)
+	if !ok {
+		return "", fmt.Errorf("error extracting time from map")
+	}
+	tempSlice, ok := data.Hourly["temperature_2m"].([]any)
+	if !ok {
+		return "", fmt.Errorf("error extracting temperature from map")
+	}
+
+	for i := 0; i < len(timeSlice) && i < len(tempSlice); i++ {
+		time, ok := timeSlice[i].(string)
+		if !ok {
+			return "", fmt.Errorf("error converting time element to string")
+		}
+		temp, ok := tempSlice[i].(float64)
+		if !ok {
+			return "", fmt.Errorf("error converting temperature element to float64")
+		}
+		msg += fmt.Sprintf("%s: %.1f\n", time, temp)
+	}
+
+	return msg, nil
 }
